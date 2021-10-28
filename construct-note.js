90cd5b19bcd0ed508b0929de5f5b8bf15166c19e
Daniel Bae <danibae91@icloud.com>
2021-10-28 12:09:04 -0700

    submit code for construct-note.js

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,37 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+// - accept message, letters
+// - function returns true if message can be built with given letters 
+// - otherwise returns false 
+
+function constructNote(message, letters) {
+    // initialize frequencyCounter helper function
+    const frequencyCounter = (str) => {
+        // intialize empty Map
+        let frequencies = new Map() 
+        for(let val of str) {
+            let valCount = frequencies.get(val) || 0
+            frequencies.set(val, valCount + 1) 
+        }
+        return frequencies; 
+    }
+
+    if(letters === '') return false 
+    let msgFreq = frequencyCounter(message) 
+    let lettersFreq = frequencyCounter(letters) 
+    // console.log(msgFreq, lettersFreq)
+
+    if(lettersFreq.size < msgFreq.size) return false 
+
+    for(let char of msgFreq.keys()) {
+        if(!lettersFreq.has(char) || msgFreq.get(char) > lettersFreq.get(char)) {
+            return false 
+        }
+    }
+    return true; 
 }
+
+console.log(constructNote('abc', 'abcd')) 
+console.log(constructNote('aabbcc', 'bcabcaddff'))
+console.log(constructNote('aa', 'abc'))
+
+module.exports = constructNote;
