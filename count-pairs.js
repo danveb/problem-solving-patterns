349409549c1d10444d8e71bc055d8628dee7437c
Daniel Bae <danibae91@icloud.com>
2021-10-29 20:18:06 -0700

    submit code and working tests

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,40 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+/* Given an array of integers, and a number, find the number of pairs of integers in the array whose sum is equal to the second parameter. You can assume that there will be no duplicate values in the array. */ 
+
+const countPairs = (array, num) => {
+    // initialize count, start/end pointers
+    let count = 0
+    let start = 0
+    let end = array.length - 1
+
+    // array sort? 
+    array.sort((a, b) => a - b)
+
+    // while loop
+    while(start < end) {
+        // create var for sum 
+        let sum = array[start] + array[end] 
+        // check if sum === target num 
+        if(sum === num) {
+            // we found it so we can move start + 1 and end - 1
+            count++
+            start++
+            end--
+        } else if(sum < num) {
+            start++
+            // else sum can be greater than num 
+        } else {
+            end--
+        }
+    }
+    return count; 
 }
+
+console.log(countPairs([3,1,5,4,2], 6))
+console.log(countPairs([10,4,8,2,6,0], 10)) 
+console.log(countPairs([4,6,2,7], 10))
+console.log(countPairs([1,2,3,4,5], 10)) 
+console.log(countPairs([1,2,3,4,5], -3)) 
+console.log(countPairs([0,-4],-4)) 
+console.log(countPairs([1,2,3,0,-1,-2],0))
+
+module.exports = countPairs; 
--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,46 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+/* Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing. */
+
+const isSubsequence = (str1, str2) => {
+    // check if there are no strings then returns false 
+    if(!str1 || !str2) return false 
+
+    // left and right pointers for both strings
+    let leftStr1 = 0
+    let rightStr1 = str1.length - 1
+    let leftStr2 = 0
+    let rightStr2 = str2.length - 1
+
+    // while loop: left2 <= right2
+    while(leftStr2 <= rightStr2) {
+        // check if str1(left) is equal to str2(left) 
+        // - and check if str1(right) is equal to str2(right) 
+        if(str1[leftStr1] === str2[leftStr2] && str1[rightStr1] === str2[rightStr2]) {
+            console.log(leftStr1, leftStr2, rightStr1, rightStr2)
+            if(leftStr1 === rightStr1 || leftStr1 === rightStr1 -1) {
+                // it matches
+                console.log('Done') 
+                return true 
+            }
+            // move pointers
+            leftStr1++
+            rightStr1--
+            leftStr2++
+            rightStr2--
+        } else if(str1[leftStr1] !== str2[leftStr2]) {
+            console.log(leftStr1, leftStr2)
+            leftStr2++ 
+        } else if(str1[rightStr1] !== str2[rightStr2]) {
+            console.log(rightStr1, rightStr2)
+            rightStr2-- 
+        }
+    }
+    return false; 
+
 }
+
+// console.log(isSubsequence('hello', 'hello world'))
+console.log(isSubsequence('sing', 'sting'))
+// console.log(isSubsequence('abc', 'abracadabra'))
+// console.log(isSubsequence('abc', 'acb'))
+
+module.exports = isSubsequence; 
